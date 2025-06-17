737e7ce8957da2793dc2e5b32e5824c2a5ac8803
Lins <linssenai01@gmail.com>
2025-06-17 11:18:10 -0300

    set database model with sqlalchemy orm

---
+++ sqlalchemy/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer as Int, String as Str, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Int, primary_key=True, index=True)
+    username = Column(Str, unique=True, nullable=False,index=True)
+    hashd_password = Column(Str, nullable=False)
+
+    passwords = relationship("PasswordEntry", back_populates="owner", cascade="all, delete-orphan")
+
+class PasswordEntry(Base):
+    __tablename__ = "passwords"
+
+    id = Column(Int, primary_key=True, index=True)
+    owner_id = Column(Int, ForeignKey("users.id"), nullable=False)
+    login = Column(Str)
+    encrypted_password = Column(Str, nullable=False)
+    note = Column(Str)
+
+    owner = relationship("User", back_populates="passwords")
